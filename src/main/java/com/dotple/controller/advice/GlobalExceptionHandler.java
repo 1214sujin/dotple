5cfec75518203c407e870b00577c1833274a94c0
Sujin Kang <1214sujin@naver.com>
2024-07-28 18:16:22 +0900

    fix: 이미 핸들러가 정의된 예외의 핸들러를 재정의

--- src/main/java/com/dotple/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/com/dotple/controller/advice/GlobalExceptionHandler.java
@@ -16,7 +16,7 @@ import java.util.NoSuchElementException;
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler
-	public ResponseEntity<ResponseMessage<String>> HandleCustomException(CustomException e) {
+	public ResponseEntity<Object> HandleCustomException(CustomException e) {
 
 		e.printStackTrace();
 		ResponseCode responseCode = e.getResponseCode();
@@ -26,31 +26,31 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 	// Optional.get()에 대한 예외 처리
 	@ExceptionHandler
-	public ResponseEntity<ResponseMessage<String>> HandleNoSuchElementException(NoSuchElementException e) {
+	public ResponseEntity<Object> HandleNoSuchElementException(NoSuchElementException e) {
 		e.printStackTrace();
 		return handleExceptionInternal(ResponseCode.C4040);
 	}
 
 	// 적절하지 않은 데이터가 DB로 들어갈 뻔한 경우. 발생해서는 안 됨!
 	@ExceptionHandler
-	public ResponseEntity<ResponseMessage<String>> HandleDataIntegrityViolationException(DataIntegrityViolationException e) {
+	public ResponseEntity<Object> HandleDataIntegrityViolationException(DataIntegrityViolationException e) {
 		e.printStackTrace();
 		return handleExceptionInternal(ResponseCode.C5001);
 	}
 
-	private ResponseEntity<ResponseMessage<String>> handleExceptionInternal (ResponseCode responseCode) {
+	// 요청 값이 무효한 경우
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		return handleExceptionInternal(ResponseCode.C4000);
+	}
+
+	private ResponseEntity<Object> handleExceptionInternal (ResponseCode responseCode) {
 		return ResponseEntity.status(responseCode.getStatus()).body(new ResponseMessage<>(responseCode, ""));
 	}
 
 	@Override
-	public ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode status, WebRequest request){
-
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode status, WebRequest request){
 		ex.printStackTrace();
-		// 요청 값이 무효한 경우
-		if (ex.getClass() == MethodArgumentNotValidException.class) {
-			return ResponseEntity.badRequest().body(new ResponseMessage<>(ResponseCode.C4000, ""));
-		}
-		
 		return ResponseEntity.status(500).body(new ResponseMessage<>(ResponseCode.C5000, ""));
 	}
 }
